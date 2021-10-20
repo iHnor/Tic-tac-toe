ce3633bdbbe9f7f5a73fd23b79828a5a52f95a39
Ihor <igormykhno@gmail.com>
2021-10-20 11:31:34 +0300

    Create class Game, create class PrintGameField and fill them with methods

--- tic-tac-toe/Program.cs
+++ tic-tac-toe/Program.cs
@@ -4,146 +4,169 @@ namespace tic_tac_toe
 {
     class Program
     {
-        static void Main()
+        class Game
         {
-            char[,] field = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
-            int[,] steps = { { 0, 0 }, { 0, 1 }, { 1, 1 }, { 0, 2 }, { 2, 2 }};
-
-            ShowTheField(field);
-
-            for (int i = 0; i < steps.GetUpperBound(0) + 2; i++)
+            private PrintGameField Print;
+            public void StartGame(char[,] field, int[,] steps)
             {
-                int testwin = isWin(field);
-                if (testwin == ' ')
+                Print.ShowTheField(field);
+                for (int i = 0; i < steps.GetUpperBound(0) + 2; i++)
                 {
-                    if (i % 2 == 0)
+                    int testwin = isWin(field);
+                    if (testwin == ' ')
                     {
-                        field = DoStep(field, steps[i, 0], steps[i, 1], 'x');
+                        if (i % 2 == 0)
+                        {
+                            field = DoStep(field, steps[i, 0], steps[i, 1], 'x');
+                        }
+                        else
+                        {
+                            field = DoStep(field, steps[i, 0], steps[i, 1], 'o');
+                        }
+                        Print.ShowTheField(field);
+                    }
+                    else if (testwin == 'x')
+                    {
+                        System.Console.WriteLine("Win: X");
+                        break;
+                    }
+                    else if (testwin == 'o')
+                    {
+                        System.Console.WriteLine("Win: O");
+                        break;
                     }
                     else
                     {
-                        field = DoStep(field, steps[i, 0], steps[i, 1], 'o');
+                        System.Console.WriteLine("No one win!");
+                        break;
                     }
-                    ShowTheField(field);
                 }
-                else if (testwin == 'x')
+            }
+
+            private char[,] DoStep(char[,] field, int row, int col, char XorO)
+            {
+                if (XorO == 'x' || XorO == 'o')
                 {
-                    System.Console.WriteLine("Win: X");
-                    break;
-                }
-                else if (testwin == 'o')
-                {
-                    System.Console.WriteLine("Win: O");
-                    break;
+                    if (checkCell(row, col, field))
+                    {
+                        field[row, col] = XorO;
+                        return field;
+                    }
+                    else
+                    {
+                        System.Console.WriteLine("Cell is not free");
+                        return field;
+                    }
                 }
                 else
                 {
-                    System.Console.WriteLine("No one win!");
-                    break;
-                }
-            }
-        }
-        static char[,] DoStep(char[,] field, int row, int col, char XorO)
-        {
-            if (XorO == 'x' || XorO == 'o')
-            {
-                if (checkCell(row, col, field))
-                {
-                    field[row, col] = XorO;
-                    return field;
-                }
-                else
-                {
-                    System.Console.WriteLine("Cell is not free");
+                    System.Console.WriteLine("Enter the right value");
                     return field;
                 }
             }
-            else
+
+            private bool checkCell(int row, int col, char[,] field)
             {
-                System.Console.WriteLine("Enter the right value");
-                return field;  
+                if (field[row, col] == ' ') return true;
+                else return false;
             }
-        }
-
-        static bool checkCell(int row, int col, char[,] field)
-        {
-            if (field[row, col] == ' ') return true;
-            else return false;
-        }
-        static char isWin(char[,] field)
-        {
-            string isX = "xxx";
-            string isO = "ooo";
-            if (isDiagonal(field, isX)) return 'x';
-            else if (isDiagonal(field, isO)) return 'o';
-            else if (isHorisontal(field, isX)) return 'x';
-            else if (isHorisontal(field, isO)) return 'o';
-            else if (isVertical(field, isX)) return 'x';
-            else if (isVertical(field, isO)) return 'o';
-            else if (isСellsAreFree(field)) return ' ';
-            else return 'y';
-        }
-
-
-        static bool isСellsAreFree(char[,] field)
-        {
-            for (int i = 0; i < 3; i++)
+            private char isWin(char[,] field)
             {
-                for (int j = 0; j < 3; j++)
+                string isX = "xxx";
+                string isO = "ooo";
+                if (isDiagonal(field, isX)) return 'x';
+                else if (isDiagonal(field, isO)) return 'o';
+                else if (isHorisontal(field, isX)) return 'x';
+                else if (isHorisontal(field, isO)) return 'o';
+                else if (isVertical(field, isX)) return 'x';
+                else if (isVertical(field, isO)) return 'o';
+                else if (isСellsAreFree(field)) return ' ';
+                else return 'y';
+            }
+
+
+            private bool isСellsAreFree(char[,] field)
+            {
+                for (int i = 0; i < 3; i++)
                 {
-                    if (field[i, j] == ' ') return true;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        if (field[i, j] == ' ') return true;
+                    }
                 }
+                return false;
             }
-            return false;
-        }
 
-        static bool isVertical(char[,] field, string XorO)
-        {
-            for (int i = 0; i < 3; i++)
+            private bool isVertical(char[,] field, string XorO)
             {
-                string resultLine = null;
-                for (int j = 0; j < 3; j++)
+                for (int i = 0; i < 3; i++)
                 {
-                    resultLine = resultLine + field[j, i];
-                    if (resultLine == XorO) return true;
+                    string resultLine = null;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        resultLine = resultLine + field[j, i];
+                        if (resultLine == XorO) return true;
+                    }
                 }
+                return false;
             }
-            return false;
-        }
-        static bool isDiagonal(char[,] field, string XorO)
-        {
-            if ($"{field[0, 0]}{field[1, 1]}{field[2, 2]}" == XorO) return true;
-            else if ($"{field[0, 2]}{field[1, 1]}{field[2, 0]}" == XorO) return true;
-            else return false;
-        }
-        static bool isHorisontal(char[,] field, string XorO)
-        {
-            for (int i = 0; i < 3; i++)
+            private bool isDiagonal(char[,] field, string XorO)
             {
-                string resultLine = null;
-                for (int j = 0; j < 3; j++)
-                {
-                    resultLine = resultLine + field[i, j];
-                    if (resultLine == XorO) return true;
-                }
+                if ($"{field[0, 0]}{field[1, 1]}{field[2, 2]}" == XorO) return true;
+                else if ($"{field[0, 2]}{field[1, 1]}{field[2, 0]}" == XorO) return true;
+                else return false;
             }
-            return false;
+            private bool isHorisontal(char[,] field, string XorO)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    string resultLine = null;
+                    for (int j = 0; j < 3; j++)
+                    {
+                        resultLine = resultLine + field[i, j];
+                        if (resultLine == XorO) return true;
+                    }
+                }
+                return false;
+            }
         }
 
-        static void ShowTheField(char[,] field)
+        class PrintGameField
         {
-            string show = transformField(field);
-            Console.WriteLine(show);
-        }
+            public void ShowTheField(char[,] field)
+            {
+                string show = transformField(field);
+                Console.WriteLine(show);
+            }
 
-        static string transformField(char[,] field)
+            private string transformField(char[,] field)
+            {
+                string transform = null;
+
+                for (int i = 0; i < field.GetUpperBound(0) + 1; i++)
+                    transform += $"{field[i, 0]}|{field[i, 1]}|{field[i, 2]} \n";
+
+                return transform;
+            }
+            public void PrintWiner()
+            {
+
+            }
+        }
+        static void Main()
         {
-            string transform = null;
+            char[,] field = { { ' ', ' ', ' ' }, { ' ', ' ', ' ' }, { ' ', ' ', ' ' } };
+            int[,] steps = { { 0, 0 }, { 0, 1 }, { 1, 1 }, { 0, 2 }, { 2, 2 } };
 
-            for (int i = 0; i < field.GetUpperBound(0) + 1; i++)
-                transform += $"{field[i, 0]}|{field[i, 1]}|{field[i, 2]} \n";
+            Game firstGame = new Game();
+            firstGame.StartGame(field, steps);
 
-            return transform;
+
+
+
         }
+
+
+
     }
 }
